ca71c51f043ebef78557b82f3697e057714b6708
Eugene Fedyakin <eugene.a.fed@gmail.com>
2025-03-06 19:21:08 +0300

    Уменьшенный файл db.json

--- calc.py
+++ calc.py
@@ -1,11 +1,13 @@
 import json
 
+FILE = 'db.json'
+
 # Открываем файл и считываем его содержимое
-with open('db.json', 'r', encoding='utf-8') as file:
+with open(FILE, 'r', encoding='utf-8') as file:
     json_data = file.read()
 
 # Подсчитываем количество символов
 symbol_count = len(json_data)
 
 # Выводим результат
-print(f"Количество символов в файле 'db.json': {symbol_count}")
+print(f"Symbol count in file '{FILE}': {symbol_count}")
